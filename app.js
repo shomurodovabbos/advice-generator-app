e2cbb921d92bd735bf691ba62afd5cc6689b0a0b
abbos91 <abbos.shomurodov91@gmail.com>
2022-04-06 15:20:58 +0900

    random joke

--- app.js
+++ app.js
@@ -3,19 +3,34 @@ const quote = document.querySelector(".quote");
 const adviceNum = document.querySelector(".advice-num");
 
 window.addEventListener("DOMContentLoaded", getQuote);
-btn.addEventListener("click", getQuote);
+btn.addEventListener("click", getQuoteDime);
 
 function getQuote(e) {
   e.preventDefault();
   const xhr = new XMLHttpRequest();
-  const random = Math.floor(Math.random() * 300);
+  //   const random = Math.floor(Math.random() * 200);
+  xhr.open("GET", "https://api.adviceslip.com/advice", true);
+  xhr.onload = function () {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      quote.innerText = `${response.slip.advice}`;
+      adviceNum.innerText = response.slip.id;
+      console.log(response.slip.advice);
+    }
+  };
+  xhr.send();
+}
+function getQuoteDime(e) {
+  e.preventDefault();
+  const xhr = new XMLHttpRequest();
+  const random = Math.floor(Math.random() * 200);
   xhr.open("GET", `https://api.adviceslip.com/advice/${random}`, true);
   xhr.onload = function () {
     if (this.status === 200) {
       const response = JSON.parse(this.responseText);
-      quote.textContent = `"${response.slip.advice}"`;
+      quote.innerText = `${response.slip.advice}`;
       adviceNum.innerText = response.slip.id;
-      console.log(response);
+      console.log(response.slip);
     }
   };
   xhr.send();
